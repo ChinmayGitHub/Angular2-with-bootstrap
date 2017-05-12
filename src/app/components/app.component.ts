ac64b4e3433533e3117c42fb5a94d7e49378caee
Sonu Sathyadas <sonusathyadas@hotmail.com>
2017-05-12 10:19:53 +0530

    Updated

--- src/app/components/app.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-  selector: 'app',
-  templateUrl: '../views/app.component.html',
-})
-export class AppComponent {
-}
--- src/app/components/home.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-  selector: 'home',
-  styleUrls: ['../styles/home.component.css'],
-  templateUrl: '../views/home.component.html'
-})
-export class HomeComponent {
-}
